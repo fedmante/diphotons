d060da7f81f414eaf87034a4f9dab2a5b6639ac4
fedmante <federica.mantegazzini@gmail.com>
2015-07-27 14:26:14 +0200

    tnp python

--- fullAnalysisRoma/python/tnpAna.py
+++ fullAnalysisRoma/python/tnpAna.py
@@ -26,7 +26,7 @@ process.source = cms.Source("PoolSource",
         # Spring15, DY
         #"file:diphotonsMicroAOD_1.root"
 
-        "/store/group/phys_higgs/cmshgg/musella/flashgg/EXOSpring15_v1/Spring15BetaV2/RSGravToGG_kMpl-001_M-500_TuneCUEP8M1_13TeV-pythia8/EXOSpring15_v1-Spring15BetaV2-v0-RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/150717_101901/0000/diphotonsMicroAOD_1.root"
+        "root://eoscms//eos/cms/store/group/phys_higgs/cmshgg/musella/flashgg/EXOSpring15_v1/Spring15BetaV2/RSGravToGG_kMpl-001_M-500_TuneCUEP8M1_13TeV-pythia8/EXOSpring15_v1-Spring15BetaV2-v0-RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/150717_101901/0000/diphotonsMicroAOD_1.root"
         )
                             )
 
